e12dae208c179072f9a7c3dec8109ad00e296722
Romanov55 <Romanov20let@gmail.com>
2023-02-02 18:47:46 +0600

    up form

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const searchInput = document.getElementById('search-input');
-const searchButton = document.getElementById('search-button');
-const searchResults = document.getElementById('search-results');
+const result = document.querySelector('.search-results')
 
-searchButton.addEventListener('click', function() {
-  const searchTerm = searchInput.value;
-
-
-  searchResults.innerHTML = `"`;
-});
+fetch('https://dummyjson.com/products')
+  .then((response) => {
+    return response.json();
+   })
+  .then((article) => {
+    console.log(article.products[1].title)
+    result.innerHTML = article.products[1].title
+  })
+  
