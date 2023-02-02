6e1d1ac83f203f4f715b74fb608fb288311d908e
Romanov55 <Romanov20let@gmail.com>
2023-02-02 16:24:48 +0600

    up index.js

--- index.js
+++ index.js
@@ -5,28 +5,6 @@ const searchResults = document.getElementById('search-results');
 searchButton.addEventListener('click', function() {
   const searchTerm = searchInput.value;
 
-  // Use searchTerm to search for products (e.g. send an API request)
-  // ...
 
-  // Update search results
-  searchResults.innerHTML = `Results for "${searchTerm}"`;
-});
-
-
-// Обработчик события submit формы
-document.getElementById("search-form").addEventListener("submit", function(event) {
-  event.preventDefault();
-
-  // Получение значения поля ввода
-  var searchTerm = document.querySelector("item-serch").value;
-
-  // Отправка запроса на сервер с помощью fetch API
-  fetch("http://127.0.0.1:5500/" + searchTerm)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(results) {
-      // Обработка результатов поиска
-      searchResults.innerHTML = (results);
-    });
+  searchResults.innerHTML = `"`;
 });
